afb54dd126be70cbb2be94e5e7576417b7ac1eb1
Abdallah Altahaineh <abdullahmahdialtahaineh777@gmail.com>
2026-03-26 17:10:51 -0700

    added comments for the constructor and added three instance methods to the FlashCard.java file.

--- app/src/main/java/org/example/FlashCard.java
+++ app/src/main/java/org/example/FlashCard.java
@@ -6,6 +6,10 @@ public class FlashCard {
     private String answer;
     private String topic;
     private boolean fliped;
+// constructs a new FlashCard object
+    // initializes a question, answer, and a topic for the FlashCard.
+    // parameters: question, answer, topic.
+    // throws an IllegalArgumentException if any of the parameters is null.
     public FlashCard(String question, String answer, String topic) {
         if (question.equals("null") || answer.equals("null") || topic.equals("null")) {
             throw new IllegalArgumentException("The question, answer, or topic is invalid.");
@@ -14,5 +18,36 @@ public class FlashCard {
         this.answer = answer;
         this.topic = topic;
         this.fliped = false;
+    }
+    // changes the current question on the FlashCard to a new one.
+    // parameters: newQuestion the new question to be added.
+    // throws an IllegalArgumentException if newQuestion is null.
+    public void changeQuestion(String newQuestion) {
+        if (newQuestion.equals("null")) {
+            throw new IllegalArgumentException("Question is invalid.");
+        }
+        this.question = newQuestion;
+    }
+    // changes the current answer on the FlashCard to a new one.
+    // parameters: newAnswer the new answer to be added.
+    // throws an IllegalArgumentException if newAnswer is null.
+    public void changeAnswer(String newAnswer) {
+        if (newAnswer.equals("null")) {
+            throw new IllegalArgumentException("The answer is invalid.");
+        }
+        this.answer = newAnswer;
+    }
+    // flips the FlashCard from question to answer and vice versa
+    // depending on the current state of the FlashCard.
+    // flips to the answer if the FlashCard is currently on the question and vice versa.
+    // returns the answer if the FlashCard flips to it, or question if the FlashCard flips to it.
+    public String flip() {
+        if (this.fliped == false) {
+            this.fliped = true;
+            return this.answer;
+        } else {
+            this.fliped = false;
+            return this.question;
+        }
     }
 }
